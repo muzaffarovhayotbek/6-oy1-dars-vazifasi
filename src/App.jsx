3b5ca024fa97d6a63e3d461d00a2414bf9cc05e9
muzaffarovhayotbek <hayotbek0511@gmail.com>
2024-11-19 23:40:22 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -43,36 +43,80 @@ function App() {
       </main>
       <div>
         <section className="section">
-          <div className="section-title">
-            <h2 className="section-title-title">
-              Build & manage distributed teams like no one else.
-            </h2>
-          </div>
-          <div className="section-main">
-            <div className="section-main-first">
-              <h2>Experienced Individuals</h2>
-              <p>
-                Our network is made up of highly experienced professionals who
-                are passionate about what they do.
-              </p>
+          <div className="container section__container">
+            <div className="section-title">
+              <h2 className="section-title-title">
+                Build & manage distributed teams like no one else.
+              </h2>
             </div>
+            <div className="section-main">
+              <div className="section-main-first">
+                <h2>Experienced Individuals</h2>
+                <p>
+                  Our network is made up of highly experienced professionals who
+                  are passionate about what they do.
+                </p>
+              </div>
 
-            <div className="section-main-two">
-              <h2>Easy to Implement</h2>
-              <p>
-                Our processes have been refined over years of implementation
-                meaning our teams always deliver.
-              </p>
-            </div>
-            <div className="section-main-three">
-              <h2>Enhanced Productivity</h2>
-              <p>
-                Our customized platform with in-built analytics helps you manage
-                your distributed teams.
-              </p>
+              <div className="section-main-two">
+                <h2>Easy to Implement</h2>
+                <p>
+                  Our processes have been refined over years of implementation
+                  meaning our teams always deliver.
+                </p>
+              </div>
+              <div className="section-main-three">
+                <h2>Enhanced Productivity</h2>
+                <p>
+                  Our customized platform with in-built analytics helps you
+                  manage your distributed teams.
+                </p>
+              </div>
             </div>
           </div>
         </section>
+
+        <article className="article">
+          <div className="container article__container">
+            <div className="article-title">
+              <h2>
+                Delivering real results for top companies. Some of our{' '}
+                <span>success stories.</span>
+              </h2>
+            </div>
+
+            <div className="article-main">
+              <div className="article-main-first">
+                <p>
+                  “The team perfectly fit the specialized skill set required.
+                  They focused on the most essential features helping us launch
+                  the platform eight months faster than planned.”
+                </p>
+                <h2>Kady Baker</h2>
+                <h3>Product Manager at Bookmark</h3>
+              </div>
+
+              <div className="article-main-two">
+                <p>
+                  “We needed to automate our entire onboarding process. The team
+                  came in and built out the whole journey. Since going live,
+                  user retention has gone through the roof!”
+                </p>
+                <h3>Aiysha Reese</h3>
+                <h4>Founder of Manage</h4>
+              </div>
+              <div className="article-main-three">
+                <p>
+                  “Amazing. Our team helped us build an app that delivered a new
+                  experience for hiring a physio. The launch was an instant
+                  success with 100k downloads in the first month.”
+                </p>
+                <h2>Arthur Clarke</h2>
+                <h3>Co-founder of MyPhysio</h3>
+              </div>
+            </div>
+          </div>
+        </article>
       </div>
     </div>
   );
